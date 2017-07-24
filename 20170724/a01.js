2c16de0d2d2bc736a5748a02144ca97c7a68d9f1
count99 <eli_hans@icloud.com>
2017-07-24 16:38:24 +0800

    ....

---
+++ 20170724/a01.js
@@ -0,0 +1,11 @@
+// alert("Hi\r123\r");
+// console("Hello world");
+document.write("新手教學");
+
+function a01() {
+    document.getElementById("a01").innerHTML="test";
+}
+
+function a02() {
+    document.getElementById("a01").innerHTML="xxxx";
+}
